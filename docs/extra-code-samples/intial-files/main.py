1d520c8e1c2da83e4d824404f4b9b8aa3f034128
Abhigyan Tripathi <abhigyantrips@gmail.com>
2021-11-21 16:32:26 +0530

    Edit code sample for `initial-files`.

--- docs/extra-code-samples/intial-files/main.py
+++ docs/extra-code-samples/intial-files/main.py
@@ -1,12 +1,10 @@
 import disnake
 from disnake.ext import commands
 
-bot = commands.Bot(command_prefix=">")
-
+bot = commands.Bot()
 
 @bot.event
 async def on_ready():
     print("The bot is ready!")
-
 
 bot.run("YOUR_BOT_TOKEN")
