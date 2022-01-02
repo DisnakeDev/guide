458cd7772cefd2ca5e6e93423c1da2c50960f49a
Abhigyan Tripathi <abhigyantrips@gmail.com>
2022-01-02 21:42:12 +0530

    feat(intro): Shift in `links-and-credits`.

--- guide/docusaurus.config.js
+++ guide/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/abhigyantrips/disnake-guide-react',
+          editUrl: 'https://github.com/abhigyantrips/disnake-guide-react/blob/main/guide',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
