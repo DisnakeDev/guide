ba2ce0b3bdcf22d93abe6f64ab6668e7648d9cf4
Abhigyan Tripathi <abhigyantrips@gmail.com>
2022-01-06 18:50:01 +0530

    fix(theme): Wrap around `useThemeContext` to update components.
    If you load the site with light theme enabled, the message components still load in dark theme (probably something on Docusaurus's side.) So, this is a thin wrapper that will cause elements to refresh to light theme once the site is loaded. (No performance issues so far.)
    
    Also, the embed titles were somehow still white in light theme. No clue how that happens, but I included custom CSS to resolve that (for now).

--- guide/src/hooks/isDarkTheme.js
+++ guide/src/hooks/isDarkTheme.js
@@ -1,9 +1,16 @@
+import {useEffect, useState} from "react";
 import useThemeContext from '@theme/hooks/useThemeContext'
 
 export default function isDarkTheme() {
+    const [themeContext, setThemeContext] = useState({});
 
-	const {isDarkTheme} = useThemeContext();
+    const realThemeContext = useThemeContext();
+
+    useEffect(() => {
+        setThemeContext(realThemeContext)
+    }, [realThemeContext]);
+
+    const {isDarkTheme} = themeContext;
 
 	return(isDarkTheme)
-
 }
