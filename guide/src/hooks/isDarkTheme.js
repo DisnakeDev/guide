9f607c5c4f01017ee97124e043690632acbec6da
shiftinv <me@shiftinv.cc>
2024-01-16 15:40:41 +0000

    fix: use non-deprecated dark mode hook
    Docusaurus previously complained about this in dev mode:
    > `useColorMode().isDarkTheme` is deprecated. Please use `useColorMode().colorMode === "dark"` instead.

--- guide/src/hooks/isDarkTheme.js
+++ guide/src/hooks/isDarkTheme.js
@@ -1,16 +1,6 @@
-import { useEffect, useState } from 'react';
 import { useColorMode } from '@docusaurus/theme-common';
 
 export default function isDarkTheme() {
-	const [themeContext, setThemeContext] = useState({});
-
-	const realThemeContext = useColorMode();
-
-	useEffect(() => {
-		setThemeContext(realThemeContext);
-	}, [realThemeContext]);
-
-	const { isDarkTheme } = themeContext;
-
-	return isDarkTheme;
+	const { colorMode } = useColorMode();
+	return colorMode === 'dark';
 }
